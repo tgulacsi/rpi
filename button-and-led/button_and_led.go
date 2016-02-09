0e28365f58c33045a1313688965eff1860f26caf
Tamas Gulacsi <tgulacsi78+rpi@gmail.com>
2016-02-09 22:15:31 +0100

    blink it

--- button-and-led/button_and_led.go
+++ button-and-led/button_and_led.go
@@ -19,22 +19,56 @@ func main() {
 	led := rpio.Pin(24)
 	led.Output()
 
-	old := button.Read()
-	for {
-		act := button.Read()
-		if act == old {
-			time.Sleep(100 * time.Millisecond)
-			continue
-		}
-		if act == rpio.High {
-			log.Printf("Port 25 is HIGH - LED on")
+	ledCh := make(chan time.Duration)
+	go func() {
+		dCh := make(chan time.Duration)
+		go func() {
+			state := rpio.Low
+			d := time.Second
+			for {
+				select {
+				case d = <-dCh:
+				case <-time.After(d):
+				}
+				state = rpio.State((state + 1) % 2)
+				led.Write(state)
+			}
+		}()
+		led.Write(rpio.Low)
+		for d := range ledCh {
+			if d < 0 {
+				led.Write(rpio.Low)
+				continue
+			}
 			led.Write(rpio.High)
-		} else {
-			log.Printf("Port 25 is LOW - LED off")
-			led.Write(rpio.Low)
+			if d == 0 {
+				continue
+			}
+			dCh <- d
 		}
-		old = act
-		time.Sleep(100 * time.Millisecond)
-	}
+	}()
+	defer close(ledCh)
 
+	inCh := make(chan rpio.State)
+	go func() {
+		old := button.Read()
+		inCh <- old
+		for {
+			act := button.Read()
+			if act != old {
+				old = act
+				inCh <- act
+			}
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+
+	ledCh <- time.Second
+	for state := range inCh {
+		if state == rpio.High {
+			ledCh <- time.Duration(333 * time.Millisecond)
+		} else {
+			ledCh <- time.Duration(1 * time.Second)
+		}
+	}
 }
