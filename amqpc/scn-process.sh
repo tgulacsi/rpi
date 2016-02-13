5e09c0782ec4aed2d3186d5981064dd0e17971aa
Tamás Gulácsi <tgulacsi78@gmail.com>
2016-02-13 18:02:53 +0100

    scn-process: use exactimage tools

--- amqpc/scn-process.sh
+++ amqpc/scn-process.sh
@@ -1,14 +1,25 @@
 #!/bin/sh -e
-MIME=$(file -i "$1")
+set -x
+fn="$1"
+. $HOME/.profile
+MIME=$(file -i "$fn")
 if echo "$MIME" | grep -q 'image/'; then
 	echo "MIME=$MIME"
 else
 	echo "MIME=$MIME, skipping!" >&2
 	exit 0
 fi
-nice -n 10 unpaper "$1" "$1"-unpapered
-rm "$1"
-gm convert "$1"-unpapered "$1".png
-rm "$1"-unpapered
-$HOME/bin/camput file -permanode -tag scan "$1".png
-rm "$1".png
+empty-page -i "$fn" | grep '^empty' && { rm "$fn"; exit 0; }
+
+optimize2bw -n -i "$fn" -o "${fn}-bw.png"
+rm "$fn"
+fn="${fn}-bw.png"
+nice -n 10 unpaper "$fn" "$fn"-unpapered
+rm "$fn"
+fn="${fn}-unpapered"
+econvert -i "$fn" -o "$fn".png
+rm "$fn"
+fn="${fn}.png"
+
+$HOME/bin/camput file -permanode -tag scan "$fn"
+rm "$fn"
