cd53e4ce92de2e63e8324abefc9a6d51d0f1f044
Tamás Gulácsi <tgulacsi78@gmail.com>
2016-02-14 10:52:45 +0100

    scn-process: zip -> pdf + gdrive

--- amqpc/scn-process.sh
+++ amqpc/scn-process.sh
@@ -1,25 +1,51 @@
 #!/bin/sh -e
+pnm2png () {
+	fn="$1"
+	D=$(cd $(dirname $fn); pwd)
+	B=$(basename $fn .pnm)
+	ofn="$D/$B-bw.png"
+	optimize2bw -n -i "$fn" -o "$ofn"
+	rm "$fn"
+	fn="$ofn"
+	empty-page -i "$fn" | grep '^empty' && { rm "$fn"; exit 0; }
+
+	ofn="$D/$B-bw-unpapered.png"
+	nice -n 10 unpaper "$fn" "$ofn"
+	rm "$fn"
+	fn="$ofn"
+
+	$HOME/bin/camput file -permanode -tag scan "$fn"
+	echo "PNG=$fn"
+}
+
+echo "### $@ ###"
+date
 set -x
 fn="$1"
 . $HOME/.profile
 MIME=$(file -i "$fn")
 if echo "$MIME" | grep -q 'image/'; then
 	echo "MIME=$MIME"
+elif echo "$MIME" | grep -q 'application/zip'; then
+	rm -rf "$fn.d"
+	mkdir -p "$fn.d"
+	cd "$fn.d"
+	unzip -jo "$fn" 
+	#rm "$fn"
+	pwd; ls -lA
+	ls | while read nm; do
+		pnm2png "$nm"
+	done
+	pwd; ls -lA
+	gm convert *.png "$fn.pdf"
+	gdrive upload -f "$fn.pdf"
+	cd $(dirname $(dirname "$fn.d"))
+	rm -rf "$fn.d"
+	exit 0
 else
 	echo "MIME=$MIME, skipping!" >&2
 	exit 0
 fi
-empty-page -i "$fn" | grep '^empty' && { rm "$fn"; exit 0; }
 
-optimize2bw -n -i "$fn" -o "${fn}-bw.png"
-rm "$fn"
-fn="${fn}-bw.png"
-nice -n 10 unpaper "$fn" "$fn"-unpapered
-rm "$fn"
-fn="${fn}-unpapered"
-econvert -i "$fn" -o "$fn".png
-rm "$fn"
-fn="${fn}.png"
-
-$HOME/bin/camput file -permanode -tag scan "$fn"
-rm "$fn"
+pnm2png "$fn"
+rm "$fn*"
