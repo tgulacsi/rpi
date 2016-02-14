49fb70caa0262939eb0cae104d0428b64694b66f
Tamás Gulácsi <tgulacsi78@gmail.com>
2016-02-14 10:54:11 +0100

    scn: zip it

--- exec-on-button/scn.sh
+++ exec-on-button/scn.sh
@@ -1,11 +1,20 @@
 #!/bin/sh -e
-FN=/tmp/scan-$$
+FN=/tmp/scan-$(date '+%Y%m%d_%H%M%S')
 set -x
+#	-S /home/pi/bin/scn-sane-pp.sh \
 scanadf \
-	-S /home/pi/bin/scn-sane-pp.sh \
 	--source 'ADF Duplex' \
-	--mode Grayscale \
-	--resolution 300 --y-resolution 300 \
+	--mode Gray \
+	--resolution 300 \
 	-o $FN-%04d.pnm
 #/home/pi/bin/camput file --permanode --tag scanner $FN*.png
-rm -rf $FN*.png
+zip -2 $FN.zip $FN*.pnm
+rm -f $FN*.pnm
+
+ENVFN="$(dirname $0)/scn-sane-pp.sh.env"
+if [ -e $ENVFN ]; then
+	# contains AMQP_USER and AMQP_PASSWORD
+	. $ENVFN
+fi
+/home/pi/bin/amqpc pub --no-compress @$FN.zip
+rm -rf $FN*
